1261d268f62f856164733804ce6efb448b2237d8
John Manack <jmnck28@gmail.com>
2021-01-13 08:33:52 -0500

    adds api to js file and get. js helper

--- js/index.js
+++ js/index.js
@@ -1,2 +1,12 @@
 'use strict';
 
+const apiKey = 'ba0d084c74eaed1c67211ce2db29a79f';
+
+function getWeather(city, state) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${state},usa&units=imperial&appid=${apiKey}`;
+    get(url).then(response => {
+        console.log(response.main.temp)
+    });
+}
+
+getWeather('statesboro','ga')
